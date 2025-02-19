df80a146d69dca005589863578d626afbe6605f7
Dawid Chorazy <dawid.chorazy@microsoft.wsei.edu.pl>
2025-02-19 17:40:33 +0100

    Fighting and fleeing mechanic is working properly

--- RPGTextGame/Program.cs
+++ RPGTextGame/Program.cs
@@ -1,4 +1,6 @@
-﻿public class Program
+﻿using System.Globalization;
+
+public class Program
 {
     public static void Main(string[] args)
     {
@@ -65,7 +67,8 @@
         Random randomOpponentOutput = new Random();
         Random randomDiceOutput = new Random();
         Random randomCrossroadOutput = new Random();
-        
+
+        string option = "";
         int randomCrossroad = randomCrossroadOutput.Next(1, 4);
         int die = randomDiceOutput.Next(1, 21);
         int enemyIndex = randomOpponentOutput.Next(1, 6);
@@ -77,16 +80,21 @@
         {
             case 1:
                 Console.WriteLine("Critical failure! You stepped into the trap!");
-
+                player.Health -= die;
                 Console.WriteLine($"You lost {die} of health points. Your current health is {player.Health - die}");
-             break; 
+                Thread.Sleep(2000);
+             break;
+            
             case 2:
             case 3:
             case 4:
-                Console.WriteLine("You found the enemy (You cannot run away nor hide)"); // only fight option
+                Console.WriteLine("You found the enemy. You cannot run away nor hide.");
+                Thread.Sleep(2000);// only fight option
                 Console.WriteLine($"You are fighting {enemy.Name} with power of {enemy.Attack} and {enemy.Health} health!");
+                Thread.Sleep(2000);
                 Fighting(player, enemy);
                 break;
+            
             case 5:
             case 6:
             case 7:
@@ -99,30 +107,49 @@
             case 12:
             case 13:
                 Console.WriteLine("Crossroads! Choose to go left or right:");
-                string cross = Console.ReadLine();
+                string cross = Console.ReadLine().ToLower();
                 if (randomCrossroad == 1)
                 {
-                    Console.WriteLine("You lost health points due to stepping into trap!");
-                    break;
+                    player.Health -= die;
+                    Console.WriteLine($"You lost {die} health points due to stepping into trap! Your current health is {player.Health}");
+                    Thread.Sleep(2000);
+                    
                 }
                 else
                 {
                     Console.WriteLine("Nothing Happens");
-                    break;
+                    Thread.Sleep(2000);
                 }
+                break;
+            
             case 14:
             case 15:
             case 16:
             case 17:
-                Console.WriteLine("You found enemy (You can attack or flee)"); // attacking enemy or fleeing
+                Console.WriteLine("You found enemy (You can fight or flee)"); // attack or flee
+                option = Console.ReadLine().ToLower();
+                if (option == "fight")
+                {
+                    Fighting(player, enemy);
+                }
+                else if (option == "flee")
+                {
+                    Flee(player, enemy);
+                }
+                Thread.Sleep(2000);
                 break;
+            
             case 18:
             case 19:
-                Console.WriteLine("You found supplies"); // default supplies
+                Console.WriteLine("You found supplies");// default supplies
+                Thread.Sleep(2000);
                 break;
+            
             case 20:
-                Console.WriteLine("You found treasure"); // better supplies
-                break; 
+                Console.WriteLine("You found treasure");// better supplies
+                Thread.Sleep(2000);
+                break;
+            
         }
     }
     }
@@ -130,29 +157,53 @@
     public static void Fighting(Player player, Enemy opp)
     {
         Random randomAttackDie = new Random();
-        int attack = randomAttackDie.Next(1, 21);
-        string option = "";
-        Console.WriteLine("Choose one:");
-        Console.WriteLine("Attack");
-        Console.WriteLine("Flee");
-        if (option == "Attack")
+        
+        while (opp.Health > 0 && player.Health > 0)
         {
             if (player.Health > 0 && opp.Health > 0)
             {
+                int attack = randomAttackDie.Next(1, 21);
                 opp.Health -= attack;
-                player.Health -= opp.Attack;
+                Console.WriteLine(
+                    $"You attacked the beast for {attack} health points! Remaining health is {opp.Health} ");
+                Thread.Sleep(2000);
+                if (opp.Health > 0)
+                {
+                    player.Health -= opp.Attack;
+                    Console.WriteLine($"You got hit for {opp.Attack}. You have {player.Health} remaining life points");
+                    Thread.Sleep(2000);
+                }
+                else if (opp.Health <= 0)
+                {
+                    Console.WriteLine("You bravely defeated monster");
+                }
             }
             else if (player.Health <= 0)
             {
                 Console.WriteLine("Unexpectedly your opponent gives you a kiss of sudden death");
+                Environment.Exit(0);
             }
             else if (opp.Health <= 0)
             {
                 Console.WriteLine("You slained your opponent");
             }
         }
-        // add flee mechanic
-        
+    }
+
+    public static void Flee(Player player, Enemy opp)
+    {
+        Random randomFleeDie = new Random();
+        int fleeDie = randomFleeDie.Next(1, 21);
+
+        if (fleeDie >= 10)
+        {
+            Console.WriteLine("You succeeded to flee");
+        }
+        else
+        {
+            player.Health -= fleeDie;
+            Console.WriteLine($"You failed to flee and lost {fleeDie} hp due to opportunist attack ");
+        }
     }
 }
 
